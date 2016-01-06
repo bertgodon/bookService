3a047daada6bdae2650589373dfdf8d5e2ab1989
V101384 <V101384@PVWP10858.PVSYS.PV.BE>
2016-01-06 21:23:50 +0100

    code cleanup

--- bookService/src/main/java/be/pvgroup/services/book/BooksServer.java
+++ bookService/src/main/java/be/pvgroup/services/book/BooksServer.java
@@ -7,19 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-/**
- * Run as a micro-service, registering with the Discovery Server (Eureka).
- * <p>
- * Note that the configuration for this application is imported from
- * {@link AccountsWebApplication}. This is a deliberate separation of concerns
- * and allows the application to run:
- * <ul>
- * <li>Standalone - by executing {@link AccountsWebApplication#main(String[])}</li>
- * <li>As a microservice - by executing {@link BooksServer#main(String[])}</li>
- * </ul>
- * 
- * @author Paul Chapman
- */
 @EnableAutoConfiguration
 @Import(BookWebApplication.class)
 public class BooksServer {
@@ -30,7 +17,6 @@ public class BooksServer {
 	protected Logger logger = Logger.getLogger(BooksServer.class.getName());
 	
 	public static void main(String[] args) {
-
 		SpringApplication.run(BooksServer.class, args);
 	}
 }
