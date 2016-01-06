72ea9b4abdc5cb6ec69586378bc4da9386610a57
V101384 <V101384@PVWP10858.PVSYS.PV.BE>
2016-01-06 21:24:47 +0100

    code cleanup

--- bookService/src/main/java/be/pvgroup/services/book/BookController.java
+++ bookService/src/main/java/be/pvgroup/services/book/BookController.java
@@ -1,6 +1,6 @@
 package be.pvgroup.services.book;
 
-import javax.inject.Inject;
+		import javax.inject.Inject;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +14,7 @@ import io.swagger.annotations.ApiParam;
 
 @Api
 @RestController
+@RequestMapping(value = "/books")
 public class BookController {
 
 	@Inject BookRepository bookRepository;
